f3014ed3af1434e897c8b4943f92480a4af33d7b
Shiv Jani <shivjani2005@gmail.com>
2025-08-28 20:04:13 +0530

    Add initial package.json and turbo.json configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@typescript-eslint/recommended',
+    'eslint:recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    es6: true
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '.next/',
+    'coverage/',
+    '*.d.ts'
+  ]
+};
